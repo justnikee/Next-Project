2078a95741e565f183be4b07aecde581e976bc69
justnikee <hk5561111212@gmail.com>
2023-04-21 01:40:32 +0530

    prisma raleway setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+  if (!res.ok) {
+    console.log(res)
+  }
+  return res.json()
+}
 
-export default function Home() {
+export default async function Home() {
+
+  const data = await getPosts()
+
+  console.log(data)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Wellcome to homepage</h2>
